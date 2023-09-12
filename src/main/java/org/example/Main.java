24c4f5eecf72b41c85e51b0a187f04fe981e8038
Ruslan Zapolianenko <rusartrade@gmail.com>
2023-09-12 22:41:17 +0300

    commit2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        OSBBMemberCrud osbbMemberCrud = new OSBBMemberCrud();
-        osbbMemberCrud.fwMigration();
-        osbbMemberCrud.getMembersWithAutoNotAllowed();
+        OSBBMember osbbMember = new OSBBMember();
+        osbbMember.fwMigration();
+        osbbMember.getMembersWithAutoNotAllowed();
     }
 }
 
--- src/main/java/org/example/OSBBMember.java
+++ src/main/java/org/example/OSBBMember.java
@@ -1,11 +1,11 @@
 package org.example;
-
+import org.apache.log4j.Logger;
 import org.flywaydb.core.Flyway;
-
 import java.sql.*;
 
-public class OSBBMemberCrud {
-    private static String url = "jdbc:mysql://localhost:3306/osbb";
+public class OSBBMember {
+
+    private static final String url = "jdbc:mysql://localhost:3306/osbb";
     private static String username = "root";
     private static String password = "Ruslan2703";
     private static Connection connection = null;
@@ -17,9 +17,7 @@ public class OSBBMemberCrud {
                 .locations("classpath:flyway/scripts")
                 .load()
                 .migrate();
-    }
-
-    public static Connection getConnection() {
+    }public static Connection getConnection() {
         if (connection == null) {
             try {
                 Class.forName("com.mysql.cj.jdbc.Driver");
@@ -30,13 +28,11 @@ public class OSBBMemberCrud {
         }
         return connection;
     }
-
     public static void getMembersWithAutoNotAllowed() {
         try {
 
             Connection connection = DriverManager.getConnection
                     (url, username, password);
-
 
             String sqlQuery = "SELECT OSBBMembers.FirstName, OSBBMembers.LastName, OSBBMembers.Email, " +
                     "Buildings.Address, Apartments.ApartmentNumber, Apartments.Area " +
@@ -46,14 +42,11 @@ public class OSBBMemberCrud {
                     "JOIN Buildings ON Apartments.BuildingID = Buildings.BuildingID " +
                     "LEFT JOIN Residents ON Apartments.ApartmentID = Residents.ApartmentID " +
                     "WHERE Residents.HasCarAccess IS NULL " +
-                    "AND (SELECT COUNT(*) FROM OwnershipRights WHERE OwnershipRights.MemberID = OSBBMembers.MemberID) < 2";
-
+                    "AND (SELECT COUNT(*) FROM OwnershipRights " +
+                    "WHERE OwnershipRights.MemberID =OSBBMembers.MemberID) <2";
 
             PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
-
-
             ResultSet resultSet = preparedStatement.executeQuery();
-
 
             while (resultSet.next()) {
                 String firstName = resultSet.getString("FirstName");
@@ -71,7 +64,6 @@ public class OSBBMemberCrud {
                 System.out.println();
             }
 
-
             resultSet.close();
             preparedStatement.close();
             connection.close();
@@ -79,8 +71,6 @@ public class OSBBMemberCrud {
             e.printStackTrace();
         }
     }
-
-
     public static void closeConnection() {
         if (connection != null) {
             try {
@@ -90,4 +80,61 @@ public class OSBBMemberCrud {
             }
         }
     }
+    public static void createMember(String firstName, String lastName, String email, String roles) {
+        try (Connection connection = getConnection()) {
+            String sql = "INSERT INTO OSBBMembers (FirstName, LastName, Email, Roles) VALUES (?, ?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, firstName);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setString(3, email);
+            preparedStatement.setString(4, roles);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void getMemberByEmail(String email) {
+        try (Connection connection = getConnection()) {
+            String sql = "SELECT * FROM OSBBMembers WHERE Email = maryn@ukr.net";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(3, email);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                int memberID = resultSet.getInt("MemberID");
+                String firstName = resultSet.getString("FirstName");
+                String lastName = resultSet.getString("LastName");
+                String roles = resultSet.getString("Roles");
+
+                System.out.println("Member ID: " + memberID);
+                System.out.println("First Name: " + firstName);
+                System.out.println("Last Name: " + lastName);
+                System.out.println("Roles: " + roles);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void updateMemberRoles(int memberID, String newRoles) {
+        try (Connection connection = getConnection()) {
+            String sql = "UPDATE OSBBMembers SET Roles = ? WHERE MemberID = 1";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, newRoles);
+            preparedStatement.setInt(1, memberID);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void deleteMember(int memberID) {
+        try (Connection connection = getConnection()) {
+            String sql = "DELETE FROM OSBBMembers WHERE MemberID = 1";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, memberID);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
